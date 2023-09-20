ea6fe3114f0a6186e98b58c43ae219af83eaed8c
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-20 12:31:33 +0500

    app page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navBar';
+import Searcher from './components/searcher';
+import Todos from './components/todos';
+let todo = [
+
+]
+
+
+
 
 function App() {
+   const [todos, setTodos] = useState(todo);
+
+   const addTodoHandler = (val) => {
+    setTodos([...todos, {id: todos.length + 1, title: val}]);
+  }
+
+  const deleteHandler = (id) => {
+    const updateTodo = todos.filter(item => item.id  !== id);
+    setTodos(updateTodo);
+  }
+
   return (
     <div className="App">
-      <Navbar />
-       
+      <Navbar  />
+      <Searcher todoFunc = {addTodoHandler} />
+       <Todos todo = {todos} todoFunc = {deleteHandler} />
+   
     </div>
   );
 }
