504274597b7fa738c13e737b728b70c502cb9a87
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-20 12:30:16 +0500

    searcher component

---
+++ src/components/searcher/index.js
@@ -0,0 +1,36 @@
+import { Input, Button, Space } from "antd";
+import { useState } from "react";
+
+export default function Searcher({ todoFunc }) {
+  const [val, setVal] = useState("");
+
+  const changeHandler = (event) => {
+    setVal(event.target.value);
+  };
+  const addHandler = () => {
+    todoFunc(val);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Space.Compact
+        style={{
+          width: "50%",
+        }}
+      >
+        <Input type="text" onChange={changeHandler} value={val} />
+        <Button
+          style={{ backgroundColor: "#f58142", color: "#fff", fontWeight: 600 }}
+          onClick={addHandler}
+        >
+          Add todo
+        </Button>
+      </Space.Compact>
+    </div>
+  );
+}
