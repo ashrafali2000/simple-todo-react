e69effeec6beccc127521aaaf89c40cd989c075d
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-20 10:11:14 +0500

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>My Todo React</h1>
+      <Navbar />
+       
     </div>
   );
 }
