8e9532a670a0fcd66d1f614375c97295252934b3
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-20 14:45:51 +0500

    delete some useless

--- src/components/todos/index.js
+++ src/components/todos/index.js
@@ -9,15 +9,3 @@ export default function Todos({todo,todoFunc}) {
     {todo.map((item) => <TodoLists key={Math.random()} style={{display:"flex", justifyContent:"space-between", alignItems:"center"}} item = {item} todoFunc={todoFunc} />) }
     </div>)
 }
-
-
-
-
-// key={Math.random()} style={{
-//     width: '50%',backgroundColor:"#ede4c7"
-//   }}>
-//    {showInput ? <p>{item.title}</p> : <input value={item.title} autoFocus style={{backgroundColor:"white"}}></input>}
-//     <div style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-//   <DeleteOutlined style={{fontSize:20,color:"#f00"}} onClick={() => todoFunc(item.id)}/>
-//   <EditOutlined  style={{fontSize:20,color:"#00f"}}  onClick={() => editHandler()}/>
-//     </div>
